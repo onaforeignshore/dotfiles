2a0e189b10e1418f126a8c243d24d5ea17b3f757
Christo Kotze <onaforeignshore@hotmail.co.uk>
2018-07-10 14:55:48 +0400

    See notes below
    - renamed exports.zsh to _exports.zsh so that it can be sourced before
    everything else (paths should be set first)
    - added LS_COLORS to colors.zsh

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -18,4 +18,11 @@ export CLICOLOR=1
 #                │ │ │ │ │ │ │ │ │ ┌──── directory writable to others, with sticky bit (ac)
 #                │ │ │ │ │ │ │ │ │ │ ┌── directory writable to other, without sticky bit (ad)  x default foreground or background
 #export LSCOLORS="exfxcxdxbxegedabagacad" # default colors
+
+#export LSCOLORS='ExGxBxDxCxEgEdxbxgxcxd' # ls colors like in Linux
+# used https://geoff.greer.fm/lscolors/ to convert them to LS_COLORS:
+#export LS_COLORS='di=1;34:ln=1;36:so=1;31:pi=1;33:ex=1;32:bd=1;34;46:cd=1;34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43'
+
 export LSCOLORS="gxgebxfxcxegedachcacad" # my colors
+# used https://geoff.greer.fm/lscolors/ to convert them to LS_COLORS:
+export LS_COLORS='di=36:ln=36;44:so=31:pi=35:ex=32:bd=34;46:cd=34;43:su=30;42:sg=37;42:tw=30;42:ow=30;43'
